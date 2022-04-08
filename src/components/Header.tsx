937c583d17fdeeb196c783862366e465d322d392
erifranck <erifranjose@hotmail.com>
2022-04-08 17:08:58 -0500

    add active styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Flex, Link as ChakraLink } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import { Flex, Link as ChakraLink, LinkProps } from "@chakra-ui/react";
+import { Link, useLocation } from "react-router-dom";
 
 const linkList: { label: string; path: string; isExternal?: boolean }[] = [
   { label: "Home", path: "/" },
@@ -8,7 +8,15 @@ const linkList: { label: string; path: string; isExternal?: boolean }[] = [
   { label: "Web Comic", path: "https://drawmy.codes/", isExternal: true },
   { label: "Blog", path: "https://medium.com/@erifranckN", isExternal: true },
 ];
+const ActiveStyles: LinkProps = {
+  border: "1px solid black",
+};
+const DefaultStyles: LinkProps = {
+  border: "none",
+};
+
 export const Header = () => {
+  const location = useLocation();
   return (
     <Flex
       border={{ md: "1px solid black", sm: "none" }}
@@ -17,41 +25,42 @@ export const Header = () => {
       boxSizing="border-box"
     >
       <Flex flexDir={{ md: "row", sm: "column" }}>
-        {linkList.map((item, index) => (
-          <ChakraLink
-            as={item.isExternal ? undefined : Link}
-            boxSizing="border-box"
-            key={index}
-            isExternal={item.isExternal}
-            _activeLink={{
-              border: { md: "none", sm: "1px solid black" },
-              borderBottom: "1px solid black",
-            }}
-            href={item.path}
-            to={item.path}
-            color="black"
-            fontSize={{ md: "24px", sm: "18px" }}
-            transition="0.6s"
-            fontFamily="abel"
-            p={{ md: "0px", sm: "10px 20px" }}
-            textAlign={{ md: "left", sm: "center" }}
-            mr={{
-              md: index < linkList.length - 1 ? "100px" : "0px",
-              sm: "0px",
-            }}
-            mb={{ md: "0px", sm: "50px" }}
-            textDecor="none"
-            _hover={{
-              border: { md: "none", sm: "1px solid black" },
-              borderBottom: "1px solid black",
-            }}
-            _active={{
-              borderBottom: "1px solid black",
-            }}
-          >
-            {item.label}
-          </ChakraLink>
-        ))}
+        {linkList.map((item, index) => {
+          const styles =
+            location.pathname === item.path ? ActiveStyles : DefaultStyles;
+          return (
+            <ChakraLink
+              as={item.isExternal ? undefined : Link}
+              boxSizing="border-box"
+              {...styles}
+              key={index}
+              isExternal={item.isExternal}
+              href={item.path}
+              to={item.path}
+              color="black"
+              fontSize={{ md: "24px", sm: "18px" }}
+              transition="0.6s"
+              fontFamily="abel"
+              p={{ md: "0px", sm: "10px 20px" }}
+              textAlign={{ md: "left", sm: "center" }}
+              mr={{
+                md: index < linkList.length - 1 ? "100px" : "0px",
+                sm: "0px",
+              }}
+              mb={{ md: "0px", sm: "50px" }}
+              textDecor="none"
+              _hover={{
+                border: { md: "none", sm: "1px solid black" },
+                borderBottom: "1px solid black",
+              }}
+              _active={{
+                borderBottom: "1px solid black",
+              }}
+            >
+              {item.label}
+            </ChakraLink>
+          );
+        })}
       </Flex>
     </Flex>
   );
