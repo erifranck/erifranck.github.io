ee15edd7b03db832a73da416b65b344f8ad97a58
erifranck <erifranjose@hotmail.com>
2022-04-07 20:30:53 -0500

    fix: resolve issues with responsive for about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,23 +1,70 @@
 import React from "react";
-import { Box, Center, Flex, HStack, Img, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Flex,
+  Icon,
+  IconButton,
+  Img,
+  Text,
+} from "@chakra-ui/react";
 import { SocialMedia } from "../components/SocialMedia";
+import { MdKeyboardArrowLeft } from "react-icons/md";
 import { Header } from "../components/Header";
+import { useLinkClickHandler } from "react-router-dom";
 
 export const About = () => {
+  const linkHandler = useLinkClickHandler<HTMLButtonElement>("/");
+
   return (
-    <Box w="100vw" h="100vh" boxSizing="border-box" pt="40px">
-      <Center h="65%">
-        <HStack h="100%" w="60%" spacing="50px">
-          <Box h="100%">
-            <Img src="/assets/pokemonTrainer.png" h="100%" />
-          </Box>
-          <Flex alignItems="space-between" h="100%" flexWrap="wrap">
-            <Box w="100%">
+    <Box
+      w="100vw"
+      h="100vh"
+      boxSizing="border-box"
+      pt="40px"
+      position="relative"
+    >
+      <IconButton
+        aria-label=""
+        icon={<Icon as={MdKeyboardArrowLeft} />}
+        bg="transparent"
+        fontSize="24px"
+        display={{ md: "none", sm: "block" }}
+        position="absolute"
+        onClick={linkHandler}
+        left="30px"
+      />
+      <Center h={{ md: "65%", sm: "min-content" }}>
+        <Flex
+          h="100%"
+          w={{ md: "60%", sm: "100%" }}
+          flexDir={{ md: "row", sm: "column" }}
+        >
+          <Flex
+            h="100%"
+            mr={{ md: "50px", sm: "0px" }}
+            w={{ md: "50%", sm: "100%" }}
+          >
+            <Img
+              src="/assets/pokemonTrainer.png"
+              m="auto"
+              d="block"
+              h={{ md: "100%", sm: "auto" }}
+              w={{ md: "auto", sm: "200px" }}
+            />
+          </Flex>
+          <Flex
+            alignItems="space-between"
+            h="100%"
+            flexWrap="wrap"
+            w={{ md: "40%", lg: "50%", sm: "100%" }}
+          >
+            <Box w="100%" p={{ md: "0px", sm: "0px 40px" }}>
               <Text
                 fontFamily="abel"
                 textTransform="uppercase"
                 fontSize="48px"
-                textAlign="left"
+                textAlign={{ md: "left", sm: "center" }}
                 mb="10px"
               >
                 Erifranck Nuñez
@@ -27,15 +74,24 @@ export const About = () => {
                 textTransform="uppercase"
                 fontSize="24px"
                 mt="10px"
-                textAlign="left"
+                mb={{ md: "20px", sm: "10px" }}
+                textAlign={{ md: "left", sm: "center" }}
               >
                 Frontend Developer
               </Text>
+              <Center
+                mb="24px"
+                alignSelf="flex-end"
+                display={{ md: "none", sm: "flex" }}
+              >
+                <SocialMedia />
+              </Center>
               <Text
                 fontFamily="abel"
                 textTransform="uppercase"
                 fontSize="16px"
                 mt="0px"
+                mb={{ md: "0px", sm: "40px" }}
                 textAlign="left"
               >
                 Hola soy Senior Frontend Developer, Ilustrador, amante de la
@@ -50,6 +106,7 @@ export const About = () => {
                 textTransform="uppercase"
                 fontSize="16px"
                 textAlign="left"
+                mb={{ md: "0px", sm: "40px" }}
               >
                 Soy Venezolano, entrenador pokemon y amante de la gastronomia,
                 asi que podemos reunirnos en un cafe o restaurante para hablar
@@ -61,18 +118,23 @@ export const About = () => {
                 textTransform="uppercase"
                 fontSize="16px"
                 textAlign="left"
+                mb={{ md: "0px", sm: "40px" }}
               >
                 Si eres un reclutador aqui puedes descargar mi CV.
               </Text>
             </Box>
-            <Box mb="24px" alignSelf="flex-end">
+            <Box
+              mb="24px"
+              alignSelf="flex-end"
+              display={{ md: "flex", sm: "none" }}
+            >
               <SocialMedia />
             </Box>
           </Flex>
-        </HStack>
+        </Flex>
       </Center>
       <Box>
-        <Center>
+        <Center display={{ md: "flex", sm: "none" }}>
           <Header />
         </Center>
       </Box>
